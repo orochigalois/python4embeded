e67072d58c18a59ac5811769c5aa5310cdb19a56
Alex Yin <fudanyinxin83@gmail.com>
2022-09-10 23:58:45 +1000

    Update fonts globally

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   purge: ["./components/**/*.js", "./pages/**/*.js"],
   darkMode: false, // or "media" or "class"
   theme: {
+    fontFamily: {
+      sans: ['ABeeZee', 'sans-serif'],
+    },
     extend: {
       colors: {
         primary: colors.indigo,
